dc66b9493fe83586eb25f290e48724d4db58c172
Amir Kalali <a.kalali@dkfz-heidelberg.de>
2021-06-09 16:14:13 +0200

    Specify package revision and fix options

--- cmake/conan_options.cmake
+++ cmake/conan_options.cmake
@@ -5,11 +5,12 @@ set(CONAN_OPTIONS ${CONAN_OPTIONS}
     poco:enable_data=False
     poco:enable_data_mysql=False
     poco:enable_data_odbc=False
+    poco:enable_data_postgresql=False
     poco:enable_data_sqlite=False
     poco:enable_json=False
+    poco:enable_jwt=False
     poco:enable_mongodb=False
     poco:enable_net=False
-    poco:enable_netssl=False
     poco:enable_netssl_win=False
     poco:enable_pagecompiler=False
     poco:enable_pagecompiler_file2page=False
@@ -17,7 +18,6 @@ set(CONAN_OPTIONS ${CONAN_OPTIONS}
     poco:enable_pocodoc=False
     poco:enable_redis=False
     poco:enable_sevenzip=False
-    poco:enable_tests=False
     poco:enable_util=False
     poco:enable_xml=False
     poco:enable_zip=False
--- cmake/conan_run.cmake
+++ cmake/conan_run.cmake
@@ -8,6 +8,7 @@ endif()
 include(${CMAKE_BINARY_DIR}/conan.cmake)
 
 conan_cmake_run(REQUIRES poco/1.10.1
+                BUILD_TYPE "None"
                 BASIC_SETUP CMAKE_TARGETS
                 GENERATORS cmake_find_package cmake_paths
                 OPTIONS ${CONAN_OPTIONS}
